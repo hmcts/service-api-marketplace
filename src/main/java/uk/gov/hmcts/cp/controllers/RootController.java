86e9b66e4355d8a88d0c71a94a836f20fa19f541
coling01 <colin.greenwood@hmcts.net>
2026-08-19 10:03:53 +0100

    feat(AMP-1018): add lombok, local postgres dev setup and improve root controller
    - Add Lombok 1.18.46 (compileOnly + annotationProcessor for main and test)
    - Add postgres:18-alpine db service to docker-compose for local dev
    - Add @Slf4j and request logging to RootController
    - Update welcome message to "Welcome to api-marketplace"

--- src/main/java/uk/gov/hmcts/cp/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/RootController.java
@@ -1,16 +1,19 @@
 package uk.gov.hmcts.cp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@Slf4j
 @RestController
 public class RootController {
 
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to spring-boot-template");
+        log.info("GET / called");
+        return ok("Welcome to api-marketplace");
     }
 }

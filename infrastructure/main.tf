33de83969f86390a8cafaee79c80cf742693cd8b
coling01 <colin.greenwood@hmcts.net>
2026-08-25 17:45:06 +0100

    fix(AMP-1018): bypass postgres subnet data source when public_access=true
    The module data source for the subnet runs when pgsql_delegated_subnet_id
    is empty, regardless of public_access. Setting it to a non-empty value
    skips the subnet lookup while public_access=true ensures it is not used.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -42,8 +42,9 @@ module "postgresql_flexible" {
   component     = var.component
   business_area = "CFT"
   location      = var.location
-  subnet_suffix = var.pgsql_subnet_suffix
-  public_access = var.pgsql_public_access
+  subnet_suffix              = var.pgsql_subnet_suffix
+  public_access              = var.pgsql_public_access
+  pgsql_delegated_subnet_id  = var.pgsql_public_access ? "bypass" : ""
 
   common_tags          = local.tags
   admin_user_object_id = var.jenkins_AAD_objectId

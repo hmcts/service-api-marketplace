62aa10b51e2838266e25fef531348639b107fa85
coling01 <colin.greenwood@hmcts.net>
2026-08-26 17:13:27 +0100

    fix(AMP-1018): fix checkstyle violations in integration tests

--- src/test/java/uk/gov/hmcts/cp/integration/AuthenticationIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/integration/AuthenticationIntegrationTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
